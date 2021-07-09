5216cb46a773595638f1c8c4fb1c7ad644e5efe4
c-masque <maudlin.oxalis@gmail.com>
2021-07-09 17:18:50 -0400

    made new Sequelize connection more dynamic

--- config/db.js
+++ config/db.js
@@ -1,11 +1,20 @@
+const config = require('config')
+const user = config.get('user')
+const password = config.get('password')
+const host = config.get('host')
+const port = config.get('port')
+
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(`postgres://postgres:nothing.@localhost:5432/`)
+console.log(user, password, host, port)
+
+const sequelize = new Sequelize(`postgres://${user}:${password}@${host}:${port}/`)
 
 async function SpiceConnect() {
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully to SpiceNet.db');
+        console.log('\n,,,,_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"~,,,,,\n')
+        console.log('_,_,_Connection has been established successfully to SpiceNet.db_,_,_');
       } catch (error) {
         console.error('Unable to connect to the SpiceNet db:', error);
     }
